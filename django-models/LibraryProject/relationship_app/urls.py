cb4e968f92935638f7f7f48a2a471e7f5b2fbcef
Jawaheryslam <orchidgraphics19@gmail.com>
2025-11-10 01:56:47 +0300

    correction for implementing user authentication in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
         path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
         #Authentication
-        path('register/', register, name='register'),
+        path('register/', views.register, name='register'),
         path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
         path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
