27568d7180ca1aff3099c1f923b67429a0ab0b5b
Jawaheryslam <orchidgraphics19@gmail.com>
2025-11-10 00:08:36 +0300

    correction for task django views and url configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from .models import Library
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'relationship_app/list_books.html',context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
