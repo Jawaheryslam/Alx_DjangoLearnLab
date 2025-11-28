6c020df67a1fb6e44591d5f99c365ab5063c4960
Jawaheryslam <orchidgraphics19@gmail.com>
2025-11-28 21:54:25 +0300

    setting up a new django project with django rest framework

---
+++ api_project/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.title
