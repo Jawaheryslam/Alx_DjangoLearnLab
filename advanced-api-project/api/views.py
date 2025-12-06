2e0544ba908d039adc59bc3fd6f481a81e7a4581
Jawaheryslam <orchidgraphics19@gmail.com>
2025-12-06 23:06:36 +0300

    correction for implementing filtering, searching and ordering in django rest framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
@@ -17,7 +17,7 @@ class BookListView(generics.ListAPIView):
 
     # Required filter backends
     filter_backends = [
-            DjangoFilterBackend,
+            rest.framework.DjangoFilterBackend,
             filters.SearchFilter,
             filters.OrderingFilter,
     ]
