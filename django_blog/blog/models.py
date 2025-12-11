929f09644c25c59ff266fa8334455c0df3ee0897
Jawaheryslam <orchidgraphics19@gmail.com>
2025-12-11 21:59:34 +0300

    creating blog post management features

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.conf import settings
+from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -23,11 +25,19 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
     author = models.ForeignKey(
             User,
             on_delete=models.CASCADE,
             related_name='posts'
     )
 
+    class Meta:
+        ordering = ['-published_date']
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.pk})
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
-from .forms import SignUpForm, UserForm, ProfileForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import SignUpForm, UserForm, ProfileForm, Postorm
 
 
 class CustomLoginView(LoginView):
@@ -45,3 +48,42 @@ def profile(request):
         'uform': uform,
         'pform': pform,
     })
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 10
+    template_name = 'blog/post_list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(published=True)
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    templte_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form): # where to rediredt on success
+        form.instance.author = self.request.user # set the author from logged in user
+        return super().form_valid(form)
+
+class AuthoreditPermissionMixin(UserPassesTestMixin):
+    def test_func(self):
+        ob = self.get_object()
+        return obj.author == self.request.user
+
+class PostUpdateView(LoginRequiredMixin, AuthorEditPermissionMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+class PostDeleteView(LoginRequiredMixin, AuthorEditPermissionMixin, Deleteview):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post-list')
