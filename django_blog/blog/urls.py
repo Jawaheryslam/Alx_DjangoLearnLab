046c0855016215b0a9a5e509530cf660dd0cbb96
Jawaheryslam <orchidgraphics19@gmail.com>
2025-12-11 22:03:25 +0300

    correction for creating blog post management features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
         path('register/', views.register, name='register'),
         path('profile/', views.profile, name='profile'),
         path('posts/', views.PostListview.as_view(), name='post-list'),
-        path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-        path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-        path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-        path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+        path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+        path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+        path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+        path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+        path('', views.PostListView.as_view(), name='post-list'),
 ]
